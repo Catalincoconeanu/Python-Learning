a8d67f8f0aebcaf572b0cc10c82f59aed0f4766a
Coconeanu <ccoconeanu@dxc.com>
2019-08-08 16:59:23 +0200

    Try and Except

---
+++ Python - Basiscs/Try and Except.py
@@ -0,0 +1,8 @@
+# Try and Except
+# Sample: Try the first statement - if first one is not working then it goes to except statement, this will print Otherwise
+try:
+    if name > 3:
+        print("Hi")
+except:
+    print("Otherwise")
+
