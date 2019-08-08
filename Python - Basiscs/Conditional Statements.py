7b8a316bdb41c778668e6c97ca257cd2f070ac80
Coconeanu <ccoconeanu@dxc.com>
2019-08-08 16:09:44 +0200

    Conditional Statements

---
+++ Python - Basiscs/Conditional Statements.py
@@ -0,0 +1,39 @@
+# If , else, elif statements
+
+if (5>3):
+    print("Hello Word")
+# This will print Hello Word
+
+if (5<3):
+    print("It is")
+else:
+    print("Is not")
+
+# This will print Is not
+
+# Comparations:
+# 3 >= 2   - greater or equal
+# 3 <= 2   - less o equal
+# 3 == 3   - equal
+# we cant use the " = " because is used to assign a value, we must use 3 == 2
+
+# Exercise1:
+
+age = 16
+if age < 13:
+    print("You are young")
+elif age >= 13  and age < 18:
+else:
+    print("You are an adult")
+
+# Exercise2:
+if 5 >3 and 2 > 1:
+    print("Hello")
+# If both are true the Hello is printed
+
+# Exercise3:
+
+if 5 > 3 or 2 > 1:
+    print("Hi")
+# If any of the statements are true the Hi message is printed
+
