7d4dbdec4b8a583a0fda2ec51ef5aa6a2c7df8de
Coconeanu <ccoconeanu@dxc.com>
2019-08-08 11:13:30 +0200

    Main

---
+++ Python - Basiscs/Variables.py
@@ -0,0 +1,21 @@
+#Variables are memory locations to stores values
+age = 30
+#We assigned the value 30 to variable "age"
+#Other Examples:
+
+sample =  "My name is Cata!"
+
+#We can also use multiple assigment
+
+sarah, bob, mike = 16,21,17
+
+#A good practice will be to assign:
+
+sarah = bob = mike = 17
+
+#this way we assigned 17 to sarah, bob and mike
+#or
+
+name,age = "Cata", 30
+
+#We assign the string with " " and the value as integer
