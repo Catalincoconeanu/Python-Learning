5558bbc85f5408ab6294ae9a28d7fa280ad39b36
Coconeanu <ccoconeanu@dxc.com>
2019-08-08 15:42:26 +0200

    Create Tuples.py

---
+++ Python - Basiscs/Tuples.py
@@ -0,0 +1,15 @@
+# Tuples is a data structure, you cannot change, update, delete items in a tuple, it uses normal paratesis
+
+tulp1 = ("orange", "apples", "bannanas")
+
+# We cannot modify the items, but we can access the values
+
+tup[1]     # Will return apples, the corresponding index
+
+# As lists we can put multiple tuples
+
+tup3 =  tup1 + tup2
+
+# We can also see the lenght
+
+len(tup3)
