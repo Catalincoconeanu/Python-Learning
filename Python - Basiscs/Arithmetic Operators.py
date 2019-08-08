477271beef87cfdbdce3a83c1c68fd9b2d8cdedf
Coconeanu <ccoconeanu@dxc.com>
2019-08-08 12:33:39 +0200

    Create Arithmetic Operators.py

---
+++ Python - Basiscs/Arithmetic Operators.py
@@ -0,0 +1,40 @@
+# Arithmetic Operators
+# For multiplication we use *
+# To divide we use /
+# % percent
+age1 = 12
+age2 = 15
+
+sum = age1 + age2
+
+# Sample of operations with strings
+
+sent1 = " Today is ok"
+sent2 = "Tomorrow is not ok"
+
+# Another sample:
+
+firstName = "Cata"
+lastName = "Coconeanu"
+
+firstName + lastName
+# Will return Cata Coconeanu
+
+# We cant substract a string from a string, but we can multiply a string with an integer
+
+i = "Hello"
+i * 10
+# This will return Hello 10 times
+
+# We can select based strings based on Index
+
+sentence = "Cata is programming!"
+sentence[0]  # This will return the first character of the string > "C"
+
+# To select a specific range of characters:
+sentence[0:3] # This will return the string "Cata"
+
+# To select from the last character of the strings to the firs ones
+
+sentence[:-1]  # This will select the last character of the string
+
