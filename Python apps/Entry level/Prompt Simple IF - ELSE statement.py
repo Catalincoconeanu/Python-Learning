958a92ff88f2dc7e01e45c925b95f92dca5ef5f3
Catalin Coconeanu <catalincoconeanu@gmail.com>
2019-10-27 16:10:12 +0100

    Entry level

---
+++ Python apps/Entry level/Prompt Simple IF - ELSE statement.py
@@ -0,0 +1,9 @@
+#Sample of simple if - else statement
+#This app will ask you for a nr then compare it with nr 5, if is bigger then 5 will print first block of code if not will print the second.
+x = input("Enter a nr: ")
+y = int(x)
+if y > 5:
+    print("The nr is greater then 5")
+else:
+    print("The nr is smaller then 5")
+print("All Done!")
