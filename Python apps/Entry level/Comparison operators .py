6a5a6c07ed6f1344bb3cbfaef050735ef7d156c7
Catalin Coconeanu <catalincoconeanu@gmail.com>
2019-10-27 15:53:30 +0100

    Entry level

---
+++ Python apps/Entry level/Comparison operators .py
@@ -0,0 +1,10 @@
+#Comparison operators samples
+x = 5
+if x == 5:
+    print("Is equal to 5")
+if x < 10:
+    print("Is less then 10")
+if x > 4:
+    print("Is greater then 4")
+if x !=8:
+    print("Is not equal to 8")
