7b8bf2454c0b3942398e55f94f3ace5be732700d
Catalin Coconeanu <catalincoconeanu@gmail.com>
2019-10-27 15:46:17 +0100

    Entry level

---
+++ Python apps/Entry level/Simple WHILE statment sample.py
@@ -0,0 +1,6 @@
+#This will do a while loop until the condition x > 10 completes. Meaning each time is looping is taking -1 from the main nr 29.
+x = 29
+while x > 10:
+    print(x)
+    x = x - 1
+print("App ends here!")
